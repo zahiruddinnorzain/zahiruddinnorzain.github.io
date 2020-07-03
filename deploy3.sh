8fe1c0e4e5a0b0f7562c8f98d3adfd00e6611551
zahiruddin <kepaknaga@gmail.com>
2020-07-03 18:09:11 +0800

    init deploy3

---
+++ deploy3.sh
@@ -0,0 +1,30 @@
+# stop nginx
+sudo service nginx stop
+printf "STOP NGINX SERVICE\n"
+
+cd ssipr-php-laravel
+sudo cp .env ../.env
+sudo git checkout .
+sudo git pull
+sudo git status
+sudo cp .env ssipr-php-laravel/.env
+printf "DONE UPDATE REPO AND ENV FILE\n"
+
+sudo composer install
+printf "DONE COMPOSER INSTALL\n"
+
+sudo composer dumpautoload
+# php artisan migrate:fresh
+# php artisan db:seed
+php artisan passport:install
+php artisan storage:link
+sudo chmod -R 777 storage
+printf "DONE CONFIGURATION ARTISAN\n"
+
+printf "STARTING NGINX SERVICE\n"
+sudo service nginx start
+
+# printf "Please edit .env file\n"
+# printf "then, start your NGINX\n"
+printf "-----ALL DONE-----\n"
+
