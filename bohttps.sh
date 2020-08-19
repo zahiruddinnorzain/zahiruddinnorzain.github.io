305f961357172ca8dacd0d27f83bcfabd5653d2d
zahiruddin <kepaknaga@gmail.com>
2020-08-19 11:57:00 +0800

    bohttps

---
+++ bohttps.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+printf "=====bohttps V 1.0=====\n"
+printf "make sure you install nginx first\nkepaknaga@gmail.com\n"
+printf "SHOW SSL VERSION\n"
+sudo openssl version -a
+
+printf "INSTALL OPENSSL\n"
+printf "continue..\n"
+
+printf "GENERATE SSL\n"
+cd /etc/nginx/
+mkdir ssl
+sudo openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/nginx/ssl/generated-private-key.key -out /etc/nginx/ssl/generated-csr.crt
+
+printf "=====PASTE THIS IN NGINX SITES-AVAILABLE FILE=====\n"
+# printf "ssl_certificate /etc/nginx/ssl/generate-csr.crt"
+# printf "ssl_certificate_key /etc/nginx/ssl/generated-private-key.key"
+
+wget https://raw.githubusercontent.com/zahiruddinnorzain/zahiruddinnorzain.github.io/master/nginxtext.txt
+cat nginxtext.txt
+
+printf "=====DONE, YOU CAN RUN YOUR SYMLINK=====\n"
+# sudo ln -s /etc/nginx/sites-available/laravel /etc/nginx/sites-enabled/
