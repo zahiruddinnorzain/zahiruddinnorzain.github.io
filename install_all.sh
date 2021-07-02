6e3f29617b982abdcaeabdf0e84d7bedc35fa589
zahiruddin <kepaknaga@gmail.com>
2021-07-03 07:12:57 +0800

    Add files via upload

---
+++ install_all.sh
@@ -0,0 +1,39 @@
+sudo apt-get update
+sudo apt install curl
+
+# git
+sudo apt install git
+git config --global user.name "zahiruddin"
+git config --global user.email "kepaknaga@gmail.com"
+
+# gcc
+sudo apt-get -y install gcc
+
+# postgres
+sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+
+sudo apt-get update
+sudo apt-get -y install postgresql-12
+sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
+sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
+sudo apt install pgadmin4-desktop
+
+# sublime
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+sudo apt-get install apt-transport-https
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text
+
+# snap
+sudo apt update
+sudo apt install snapd
+
+sudo snap install postman
+sudo snap install simplenote
+sudo snap install vlc
+sudo snap install mailspring
+
+
