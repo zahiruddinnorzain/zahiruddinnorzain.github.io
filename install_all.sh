d5fad2bc17186b09939b10137c3d9d8a5c4e216a
zahiruddin <kepaknaga@gmail.com>
2021-08-13 11:23:42 +0800

    Update install_all.sh

--- install_all.sh
+++ install_all.sh
@@ -19,6 +19,7 @@ sudo apt-get -y install postgresql-12
 sudo curl https://www.pgadmin.org/static/packages_pgadmin_org.pub | sudo apt-key add
 sudo sh -c 'echo "deb https://ftp.postgresql.org/pub/pgadmin/pgadmin4/apt/$(lsb_release -cs) pgadmin4 main" > /etc/apt/sources.list.d/pgadmin4.list && apt update'
 sudo apt install pgadmin4-desktop
+#sudo apt-get install libpq-dev
 
 # sublime
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
