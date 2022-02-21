f3d0a0a0759fa8a2df6de8577159275d2d141729
zahiruddin <kepaknaga@gmail.com>
2022-02-21 16:06:53 +0800

    dummy file js

---
+++ profile_dummy.js
@@ -0,0 +1,18 @@
+var dummy = {
+    "profile": { 
+        "nama":"ZAHIRUDDIN",
+        "nama_bapa":"NORZAIN",
+        "phone":"0171233234",
+        "email":"emailsaya@gmailm.com",
+    }
+};
+
+$(document).ready(function(){
+
+    // console.log(dummy["profile"]["nama"]);
+    $('#noLaporan').val(dummy["profile"]["nama"]);
+    $('#nama').val(dummy["profile"]["nama"]);
+    $('#phone').val(dummy["profile"]["phone"]);
+    $('#email').val(dummy["profile"]["email"]);
+
+});
